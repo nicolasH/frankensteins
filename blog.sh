b05cb7429526e450de90d7f4ff76503aff58d505
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2011-06-24 02:47:35 +0800

    Added comments and printouts.

--- blog.sh
+++ blog.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 echo 'My blog generating script : mostly commandline'
-#create the year directories
-#mkdir ` ls | cut -c 1-4 | sort | uniq `
-#create year + month directories
-#mkdir -p `ls -l | cut -c 46- | sed 's#\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\).*#\1/\2/#'|sort | uniq`
+echo 'Creating the directories'
+#create the year/month/day directories
+mkdir -p `ls -l blog/*.text| cut -c 46- | sed 's#\(blog\/[0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\).*#\1/\2/\3/#'|sort | uniq`
 
-#dir with year, month , day 
-DIRS=`ls -l blog/*.text| cut -c 46- | sed 's#\(blog\/[0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\).*#\1/\2/\3/#'|sort | uniq`
-
-echo "mkdir -p $DIRS"
-mkdir -p $DIRS
-
-MMD=`ls -l blog/*.text| cut -c 46- | sed 's#\(blog\/[0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)\.\(.*\)\.text#echo \1/\2/\3/ \1/\2/\3/\4\.html >> titles;head -n 1 \1\2\3\.\4.text >> titles;cat head_1 >> \1/\2/\3/\4\.html;head -n 1 \1\2\3\.\4.text >> \1/\2/\3/\4\.html;cat blog/head_2 >> \1/\2/\3/\4\.html;multimarkdown \1\2\3\.\4.text >> \1/\2/\3/\4\.html;cat footer >> \1/\2/\3/\4\.html;#'`
-echo "$MMD" >> tmp.sh
+echo 'Creating the posts'
+#creates a command for each post that: create an html page from the head_1, the title (first line) of the post, the head_2, the multimarkdowned version of the post file and the footer. Also adds the url of the generated html file and the first line of the .text file to a 'titles' file in the blog directory.
+echo `ls -l blog/*.text| cut -c 46- | sed 's#\(blog\/[0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)\.\(.*\)\.text#echo \1/\2/\3/ \1/\2/\3/\4\.html >> titles;head -n 1 \1\2\3\.\4.text >> titles;cat head_1 >> \1/\2/\3/\4\.html;head -n 1 \1\2\3\.\4.text >> \1/\2/\3/\4\.html;cat blog/head_2 >> \1/\2/\3/\4\.html;multimarkdown \1\2\3\.\4.text >> \1/\2/\3/\4\.html;cat footer >> \1/\2/\3/\4\.html;#'`>> tmp.sh
 bash tmp.sh
 rm tmp.sh
 
+echo 'Creating the index'
 cat head_1 >> blog/index.html
 echo "Blog for Niconomicon" >> blog/index.html
 cat head_2 >> blog/index.html
-
+#Transform the lines of the titles file into the Date Link Title format.
 `sed -e :a -e '/\(\.*\.html\)$/N; s/\n/\"\>/; ta' titles | sed 's:\(blog\/\)\(.\{12\}\)\.*blog\/\(.*\):\<div class=\"index\"\>\2<a href\=\"\3\<\/a\>\<\\div\>:'>>blog/index.html`
 rm titles
 cat footer >> blog/index.html
-#echo $INDEX
-#And voila!
+
+echo "And voila!"
 
