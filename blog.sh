b433c6263dbb23b420c272bab86542feb4dd3902
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2011-06-24 12:47:38 +0800

    cleaned up a bit. Still not production ready :-)

--- blog.sh
+++ blog.sh
@@ -6,14 +6,14 @@ mkdir -p `ls -l blog/*.text| cut -c 46- | sed 's#\(blog\/[0-9]\{4\}\)\([0-9]\{2\
 
 echo 'Creating the posts'
 #creates a command for each post that: create an html page from the head_1, the title (first line) of the post, the head_2, the multimarkdowned version of the post file and the footer. Also adds the url of the generated html file and the first line of the .text file to a 'titles' file in the blog directory.
-echo `ls -l blog/*.text| cut -c 46- | sed 's#\(blog\/[0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)\.\(.*\)\.text#echo \1/\2/\3/ \1/\2/\3/\4\.html >> titles;head -n 1 \1\2\3\.\4.text >> titles;cat head_1 >> \1/\2/\3/\4\.html;head -n 1 \1\2\3\.\4.text >> \1/\2/\3/\4\.html;cat blog/head_2 >> \1/\2/\3/\4\.html;multimarkdown \1\2\3\.\4.text >> \1/\2/\3/\4\.html;cat footer >> \1/\2/\3/\4\.html;#'`>> tmp.sh
+echo `ls -l blog/*.text| cut -c 46- | sed 's#\(blog\/[0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)\.\(.*\)\.text#echo \1/\2/\3/ \1/\2/\3/\4\.html >> titles;head -n 1 \1\2\3\.\4.text >> titles;cat header >> \1/\2/\3/\4\.html;head -n 1 \1\2\3\.\4.text >> \1/\2/\3/\4\.html;cat blog/body >> \1/\2/\3/\4\.html;multimarkdown \1\2\3\.\4.text >> \1/\2/\3/\4\.html;cat footer >> \1/\2/\3/\4\.html;#'`>> tmp.sh
 bash tmp.sh
 rm tmp.sh
 
 echo 'Creating the index'
-cat head_1 >> blog/index.html
+cat header >> blog/index.html
 echo "Blog for Niconomicon" >> blog/index.html
-cat head_2 >> blog/index.html
+cat body >> blog/index.html
 #Transform the lines of the titles file into the Date Link Title format.
 `sed -e :a -e '/\(\.*\.html\)$/N; s/\n/\"\>/; ta' titles | sed 's:\(blog\/\)\(.\{12\}\)\.*blog\/\(.*\):\<div class=\"index\"\>\2<a href\=\"\3\<\/a\>\<\\div\>:'>>blog/index.html`
 rm titles
