2355c99101ad1bcd2029c869933a1644401bfcc5
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2011-07-06 15:54:15 +0900

    Added basic styling

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 _The answer to your questions about blogging in 42 line of bash (and
 thousands of lines of C)._
 
->"His limbs were in proportion, and I had selected his features as
+>_"His limbs were in proportion, and I had selected his features as
 beautiful. Beautiful!--Great God! His yellow skin scarcely covered the
 work of muscles and arteries beneath; his hair was of a lustrous
 black, and flowing; his teeth of a pearly whiteness; but these
 luxuriances only formed a more horrid contrast with his watery eyes,
 that seemed almost of the same colour as the dun white sockets in
 which they were set, his shrivelled complexion and straight black
-lips." 
+lips."_ 
 - Mary Shelley, 'Frankenstein'
 
 The goal of this software is to generate a static html blog from
@@ -20,25 +20,25 @@ markdown compliant files. It is similar in aim to [jekyll][] and
 It is mostly UNIX based. The current version uses [peg-multimarkdown from
 Fletcher Peney][mmd]. 
 
-It is a horrible assemblage of bash, sed, cat, echo, date, mkdir
+It is a nameless and horrible assemblage of bash, sed, cat, echo, date, mkdir
 etc... which uses almost no variable.
 
 ### First time usage:
 
 1. install [multimarkdown][mmd].
 2. clone the project to some directory.
-3. `victor.sh init`
-4. `victor.sh new "My First Post"`
+3. `___.sh init`
+4. `___.sh new "My First Post"`
 5. `nano blog/2011_11_11.1337.My-First-Post.md`
-6. `victor.sh gen`
+6. `___.sh gen`
 7. `rsync -r blog --exclude '*.md' user@some.remote.host.net:/home/public/`
 8. you have a blog!
 
 ### Day to day usage:
 
-1. `victor.sh new "This is my new post"`
-2. `Emacs blog/yyyy\_mm\_dd_hh.mm.this-is-my-new-post.md`
-3. `victor.sh gen`
+1. `___.sh new "This is my new post"`
+2. `Emacs blog/2011_11_11.1455.This-is-my-new-post.md`
+3. `___.sh gen`
 4. `rsync -r blog --exclude '*.md' user@some.remote.host.net:/home/public/`
 
 [mmd]:http://fletcherpenney.net/multimarkdown/
--- ___.sh
+++ ___.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$1" = "init" ];then
-    echo " init        : Creating the blog directory and copies all the 'templates' files (_files) there."
-    mkdir blog; ls _* | sed 's:_\(.*\):cp _\1 blog/\1:'| bash
+    echo " init  : Creates the blog directory and copies all the 'templates' files (_files) there."
+    mkdir blog; ls _*-* | sed 's:_\(.*\):cp _\1 blog/\1:'| bash
     exit
 fi
 
@@ -14,21 +14,21 @@ if [ "$1" = "new" ];then
 fi
 
 if [ "$1" = "clean" ];then
-    echo " clean       : Removing the generated html files by removing the date directories"
+    echo " clean : Removes the generated html files by removing the date directories"
     cd blog
     rm -r `ls *.md | cut -c 1-10 | sed 's#_#/#g'|sort | uniq`;rm index.html
     exit
 fi
 
 if [ "$1" = "gen" ];then
-    echo " gen         : Generating the blog"
+    echo " gen : Generates the blog"
     echo "0 - Removing the generated html files by removing the date directories"
     cd blog
     rm -r `ls *.md | cut -c 1-10 | sed 's#_#/#g'|sort | uniq`;rm index.html
     echo "1 - Creating the year/month/day directories"
     mkdir -p `ls *.md | cut -c 1-10 | sed 's#_#/#g'|sort | uniq`
     echo "2 - Creating the posts"
-    ls *.md| sed 's#\(.\{10\}\)\(.*\)\.md#echo \1 \1\2.html >> titles;head -n 1 TK\1\2.md >> titles;cat post-header >> \1\2.html;head -n 1 TK\1\2.md >> \1\2.html;cat post-body >> \1\2\.html;multimarkdown TK\1\2.md >> \1\2\.html;cat post-footer >> \1\2\.html;#' |sed 's#_#\/#g'|sed 's# TK\(....\)\/\(..\)\/\(..\)\.# \1_\2_\3.#g' | sed 's: \(....\/..\/..\)\.: \1\/:g'|bash
+    ls *.md| sed 's#\(.\{10\}\)\(.*\)\.md#echo \1 \1\2.html >> titles;head -n 1 TK\1\2.md|sed "s:\#\*::g" >> titles;cat post-header >> \1\2.html;head -n 1 TK\1\2.md |sed "s:\#\*::g" >> \1\2.html;cat post-body >> \1\2\.html;multimarkdown TK\1\2.md >> \1\2\.html;cat post-footer >> \1\2\.html;#' |sed 's#_#\/#g'|sed 's# TK\(....\)\/\(..\)\/\(..\)\.# \1_\2_\3.#g' | sed 's: \(....\/..\/..\)\.: \1\/:g'|bash
     echo "3 - Creating the index"
     cat index-header >> index.html
     cat index-body >> index.html
@@ -39,4 +39,4 @@ if [ "$1" = "gen" ];then
     echo "And voila!"
     exit
 fi
-echo "usage : victor.sh init new [title] clean gen";cat victor.sh | grep "echo \" "  | bash
+echo "usage : ___.sh init new [title] clean gen";cat ___.sh | grep "echo \" " |bash
