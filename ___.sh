0808cb4366a537f6e86dd9174847fbfde8a98e60
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2011-07-06 21:14:02 +0900

    beta 2

--- ___.sh
+++ ___.sh
@@ -8,30 +8,30 @@ if [ "$1" = "init" ];then
 fi
 
 if [ "$1" = "new" ];then
-    echo " new [title] : Creates an blog post at blog/`date +%Y_%m_%d.%H%M`.[prettified title].md ."
-    echo "$2" >> blog/`date +%Y_%m_%d.%H%M.``echo $2 |sed 's:[\ \" \, \.\:\?_]:\-:g' | sed -e "s:':\-:g" | sed -e 's:\-\-*:\-:g'| sed -e 's:\-$::;s:^\-::'`.md
+    echo " new [title] : Creates an blog post at blog/`date +%Y_%m_%d`.[prettified title].md ."
+    echo "$2" >> blog/`date +%Y_%m_%d.``echo $2 |sed 's:[\ \" \, \.\:\?_]:\-:g' | sed -e "s:':\-:g" | sed -e 's:\-\-*:\-:g'| sed -e 's:\-$::;s:^\-::'`.md
     exit
 fi
 
 if [ "$1" = "clean" ];then
-    echo "       clean : Removes the generated html files by removing the date directories"
     cd blog
-    rm -r `ls *.md | cut -c 1-10 | sed 's#_#/#g'|sort | uniq`;rm index.html
+    echo "       clean : Removes the generated html files by removing the date directories"
+    rm -r `ls *.md | cut -c 1-4 | sort | uniq`;rm index.html
     exit
 fi
 
 if [ "$1" = "gen" ];then
     echo "         gen : Generates the blog"
-    echo "0 - Removing the generated html files by removing the date directories"
     cd blog
-    rm -r `ls *.md | cut -c 1-10 | sed 's#_#/#g'|sort | uniq`;rm index.html
+    echo "0 - Removing the generated html files by removing the year directories"
+    rm -r `ls *.md | cut -c 1-4 | sort | uniq`;rm index.html
     echo "1 - Creating the year/month/day directories"
     mkdir -p `ls *.md | cut -c 1-10 | sed 's#_#/#g'|sort | uniq`
     echo "2 - Creating the posts"
     ls *.md|sort -r| sed 's#\(.\{10\}\)\(.*\)\.md#echo \1 \1\2.html >> titles;head -n 1 TK\1\2.md|sed "s:\#\*::g" >> titles;cat post-header >> \1\2.html;head -n 1 TK\1\2.md |sed "s:\#\*::g" >> \1\2.html;cat post-body >> \1\2\.html;echo "<div class=\"date\">\1</div>">> \1\2.html;multimarkdown TK\1\2.md >> \1\2\.html;cat post-footer >> \1\2\.html;#' |sed 's#_#\/#g'|sed 's# TK\(....\)\/\(..\)\/\(..\)\.# \1_\2_\3.#g' | sed 's: \(....\/..\/..\)\.: \1\/:g'|bash
     echo "3 - Creating the index"
     cat index-header index-body >> index.html
-    ls *.md|sort -r|head -n 1|sed 's#\(....\).\(..\).\(..\)\(.*\)#echo "<div class=\"post\"><div class=\"date\">\1\/\2\/\3</div>";multimarkdown \1\_\2\_\3\4;echo "</div>"#'|bash >> index.html
+    ls *.md|sort -r|head -n 1|sed 's#\(....\).\(..\).\(..\)\(.*\)#echo "<div class=\"date\">\1\/\2\/\3</div>";multimarkdown \1\_\2\_\3\4;#'|bash >> index.html
     cat index-middle >>index.html
     #Transform the lines of the titles file into the Date Link Title format.
     sed -e :a -e '1,2d; /\(\.*\.html\)$/N; s/\n/\"\>/; ta' titles | sed 's:\(.\{11\}\)\.*\(.*\):\<div class=\"index_item\"\>\1 \&raquo\; <a href\=\"\2\<\/a\>\<\/div\>:'>>index.html
